404d7d7f63201aed05532663db4fd98710e7893f
Marcel1203 <Mazaca120703@gmail.com>
2025-02-14 17:41:34 -0600

    BackEnd

---
+++ PracticaProgramada1/DAL/Implementations/PersonaDAL.cs
@@ -0,0 +1,45 @@
+﻿using Abstracciones.Abstracciones;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class PersonaDAL : IPersonaDAl
+    {
+        private PracticaProgramada1Context _context;
+        public PersonaDAL(PracticaProgramada1Context context)
+        {
+            _context = context;
+        }
+        public bool AddPersona(Persona persona)
+        {
+            try 
+            {
+                _context.Personas.Add(persona);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool DeletePersona(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Persona> GetPersonas()
+        {
+            throw new NotImplementedException();
+        }
+        public bool UpdatePersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ PracticaProgramada1/DAL/Interfaces/IPersonaDAL.cs
@@ -0,0 +1,18 @@
+﻿using Abstracciones.Abstracciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IPersonaDAl
+    {
+        bool AddPersona(Persona persona);
+        bool UpdatePersona(Persona persona);
+        bool DeletePersona(int id);
+        List<Persona> GetPersonas();
+
+    }
+}
