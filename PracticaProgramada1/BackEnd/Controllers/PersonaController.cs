ba4b44c87935ecd89923456a9f465e37ec140fe0
Marcel1203 <Mazaca120703@gmail.com>
2025-02-14 19:29:19 -0600

    DAL Gen y  Unidad

---
+++ PracticaProgramada1/BackEnd/Controllers/PersonaController.cs
@@ -0,0 +1,51 @@
+﻿using Abstracciones.Abstracciones;
+using BackEnd.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        IPersonaService _personaService;
+        public PersonaController(IPersonaService personaService)
+        {
+            _personaService = personaService;
+        }
+        // GET: api/<PersonaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PersonaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonaController>
+        [HttpPost]
+        public void Post([FromBody]Persona persona)
+        {
+            _personaService.AddPersona(persona);
+        }
+
+        // PUT api/<PersonaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<PersonaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- PracticaProgramada1/BackEnd/Program.cs
+++ PracticaProgramada1/BackEnd/Program.cs
@@ -1,3 +1,7 @@
+using Abstracciones.Abstracciones;
+using BackEnd.Services.Implementations;
+using BackEnd.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +12,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+#region DI
+builder.Services.AddDbContext<PracticaProgramada1Context>();
+builder.Services.AddScoped<IPersonaService, PersonaService>();
+
+
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
